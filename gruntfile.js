6d432ed59fd211bcac80bdf4dd4b34f86683dcf4
MingShing Su <mingshing.su@gmail.com>
2023-10-02 17:11:10 +0800

    graphcanvas

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     projectFiles: ['src/litegraph.js',
+    'src/classes/lgraphcanvas.js',
     'src/classes/contextmenu.js',
     'src/classes/curveeditor.js',
     'src/classes/helper.js',
